ae799cd1f9dc44f197c19b756ee59c4fe409a6ce
Mya Applesauce <myaapplesauce42@gmail.com>
2026-04-19 14:05:08 -0400

    added bullet image

--- settings.py
+++ settings.py
@@ -14,4 +14,6 @@ class Settings:
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (240, 60, 60)
+        self.bullet_image = pygame.image.load("images/bullet.png")
+
         self.bullets_allowed = 5
