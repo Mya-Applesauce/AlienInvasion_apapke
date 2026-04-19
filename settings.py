a3c0b302f763757ee47f92cc1d2bd74f9abdf512
Mya Applesauce <myaapplesauce42@gmail.com>
2026-04-19 12:41:47 -0400

    all pre-milestone one participation work

---
+++ settings.py
@@ -0,0 +1,14 @@
+class Settings:
+
+    def __init__(self):
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (69, 32, 136)
+        
+        self.ship_speed = 8
+
+        self.bullet_speed = 5.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (240, 60, 60)
+        self.bullets_allowed = 5
